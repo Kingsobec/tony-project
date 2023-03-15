c147d9575e44bda36afd4606670ca3cb9e190950
kingsobec <kingsobec@gmail.com>
2023-03-15 21:18:52 +0100

    changed the road map to the correct one

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -22,28 +22,27 @@ const Roadmap = () => {
         </div>
         <div className="relative px-[2rem] sm:py-[3rem] rounded-[20px] sm:w-[30%] text-center w-[80%] m-auto py-[4rem]  text-shadow-xl">
           <img src={book} alt="" className='absolute z-[-5] h-full w-full top-0 right-0'/>
-          <h4 className=" font-bold text-[1.5rem] z-20">PHASE 1</h4>
+          <h4 className=" font-bold text-[1.5rem] z-20">PHASE 2</h4>
         <ul className='z-10 text-[1.2rem]'>
-          <li>stealth launch</li>
-          <li>lock liquidity</li>
-          <li>renouce contract</li>
-          <li>community building</li>
-          <li>website deployment</li>
-          <li>100 holders</li>
-          <li>30-50k market cap</li>
+          <li>twitter marketing</li>
+          <li>telegram marketing</li>
+          <li>white paper</li>
+          <li>twitter raiding competition</li>
+          <li>500 holders</li>
+          <li>250k marketcap</li>
         </ul>
         </div>
         <div className="relative px-[2rem] sm:py-[3rem] rounded-[20px] sm:w-[30%] text-center w-[80%] m-auto py-[4rem]  text-shadow-xl">
           <img src={book} alt="" className='absolute z-[-5] h-full w-full top-0 right-0'/>
-          <h4 className=" font-bold text-[1.5rem] z-20">PHASE 1</h4>
+          <h4 className=" font-bold text-[1.5rem] z-20">PHASE 3</h4>
         <ul className='z-10 text-[1.2rem]'>
-          <li>stealth launch</li>
-          <li>lock liquidity</li>
-          <li>renouce contract</li>
-          <li>community building</li>
-          <li>website deployment</li>
-          <li>100 holders</li>
-          <li>30-50k market cap</li>
+            <li>coin gecko listing</li>
+            <li>coinmarketcap listing</li>
+            <li>press release</li>
+            <li>partnerships</li>
+            <li>utility development</li>
+            <li>1000 holders</li>
+            <li>1m marketcap</li>
         </ul>
         </div>
       </div>
